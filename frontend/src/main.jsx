d2638159011eff8eb3b9f7a84824051f877dbf00
Murilo Della Justina <161324281+Murilodellajustina@users.noreply.github.com>
2026-05-31 21:35:06 -0300

    feat: adicionando logo principal, e adicionando tela de cadastro, com algumas funcionalidades novas, além de arrumar algumas partes para ficar compativel a futura integração com backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import Login from './pages/login.jsx';
+import Cadastro from './pages/cadastro.jsx';
 
 const router = createHashRouter([
-  { path: "/", element: <Login /> }
+  { path: "/", element: <Login /> },
+  { path: "/cadastro", element: <Cadastro /> }
+
 ],
 );
 createRoot(document.getElementById("root")).render(
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,10 +1,30 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../imagens/Logo100TETO.png"
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [erro, setErro] = useState("");
+  const navigate = useNavigate();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    setErro("");
+
+    try {
+      const res = await login(email, senha);
+
+      navigate("/paginaInicial");
+
+    } catch (err) {
+      setErro("Email ou senha incorretos");
+      if (senha.length < 6) setErro("A senha deve ter 6 digitos!!");
+      console.error(err);
+    }
+  }
 
   return (
     <div className="container-fluid vh-100 d-flex justify-content-center align-items-center bg-light">
@@ -12,6 +32,12 @@ export default function Login() {
         className="card shadow p-4"
         style={{ width: "100%", maxWidth: "400px" }}
       >
+        <img
+          src={logo}
+          alt="Logo"
+          className="mx-auto d-block mb-4"
+          style={{ width: "300px" }}
+        />
         <h2 className="text-center mb-4">Login</h2>
 
 
@@ -41,9 +67,14 @@ export default function Login() {
           </div>
 
           <div className="d-grid">
-            <button className="btn btn-primary" type="button">
+            <button className="btn btn-primary" type="button" onClick={handleLogin}>
               Entrar
             </button>
+          </div>
+          <div className="text-center mt-3">
+            <p>
+              Não tem uma conta? <Link to="/cadastro">Cadastre-se</Link>
+            </p>
           </div>
         </form>
       </div>
